5ac381b5c24a90d4e1f5910a1dd09e16489d389c
640112418097 <640112418097@bru.ac.th>
2022-12-01 16:48:05 +0700

    lab4

---
+++ lab4/index.php
@@ -0,0 +1,31 @@
+<?php
+echo "Hello.PHP.!!";
+
+// phpinfo();
+
+$no = 10;
+$name = 'Win';
+
+?>
+
+<table>
+    <thead>
+        <tr>
+            <th>1</th>
+            <th>1</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        for ($i = 1; $i < 5; $i++) {
+        ?>
+            <tr>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+
+        <?php
+        }
+        ?>
+    </tbody>
+</table>
